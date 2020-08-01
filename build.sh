0b9cb8bdccf69dd775b6e188852e1fba1d6c82d9
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-08-01 19:34:26 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+PATCH_VERSION=$(cat ./VERSION)
+VERSION=0.0.$PATCH_VERSION
+OS=`uname`
+echo "Building easyproxy $VERSION"
+go build
+DIST_NAME=easyproxy-$VERSION-$OS.zip
+zip $DIST_NAME easyproxy
+echo "Built successfully and the package is located in $(pwd)/$DIST_NAME"
+
+let NEXT_VERSION=++PATCH_VERSION
+echo $NEXT_VERSION > ./VERSION
+echo "Prepare next release $NEXT_VERSION"
