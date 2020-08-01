3773d6fe9f611fc688465adfda29032aa101bfa0
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-08-01 19:53:41 +0800

    Build a fix-name distribution

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ VERSION=0.0.$PATCH_VERSION
 OS=`uname`
 echo "Building easyproxy $VERSION"
 go build
-DIST_NAME=easyproxy-$VERSION-$OS.zip
+DIST_NAME=easyproxy-$OS.zip
 zip $DIST_NAME easyproxy
 echo "Built successfully and the package is located in $(pwd)/$DIST_NAME"
 
